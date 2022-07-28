62bb49545d386a3994f257db9fff71b362bb0bcf
Daniel Martinez <dmartmar@mercadona.es>
2022-07-28 11:40:30 +0200

    feat: custom directive to destroy an element

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -13,6 +13,9 @@ export class AgregarComponent implements OnInit {
     nombre: ['', Validators.required]
   });
 
+  color: string = 'blue';
+  nombre: string = 'Dani'
+
   constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
@@ -21,6 +24,14 @@ export class AgregarComponent implements OnInit {
   isInvalidField( field: string ): boolean {
     return this.miFormulario.get(field)?.touched &&
           this.miFormulario.get(field)?.invalid || false
+  }
+
+  cambiarNombre() {
+    this.nombre = Math.random().toString();
+  }
+
+  cambiarColor() {
+    this.color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
   }
 
 }
--- src/app/shared/directives/err-msg.directive.ts
+++ src/app/shared/directives/err-msg.directive.ts
@@ -5,25 +5,51 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 })
 export class ErrMsgDirective implements OnInit {
 
+  private _color: string = 'red';
+  private _mensajeTexto: string = 'Este campo es obligatorio';
+
   htmlElement: ElementRef<HTMLElement>;
-  @Input() color: string = 'red';
-  @Input() mensajeTexto: string = 'Este campo es obligatorio';
+
+  @Input() set color( valor: string ) {
+    this._color = valor;
+    this.setStyle();
+  };
+
+  @Input() set mensajeTexto( mensaje: string ) {
+    this._mensajeTexto = mensaje;
+    this.setText();
+  }
+
+  @Input() set invalido( valor: boolean) {
+    console.log('Valor ', valor);
+
+    valor ? this.mostrarError():  this.ocultarError()
+  }
 
   constructor( private el: ElementRef<HTMLElement> ) {
     this.htmlElement = el;
   }
 
   ngOnInit(): void {
+
     this.setText();
     this.setStyle();
   }
 
   setStyle(): void {
-    this.htmlElement.nativeElement.style.color = this.color;
+    this.htmlElement.nativeElement.style.color = this._color;
     this.htmlElement.nativeElement.classList.add('form-text');
   }
 
   setText(): void {
-    this.htmlElement.nativeElement.innerText = this.mensajeTexto;
+    this.htmlElement.nativeElement.innerText = this._mensajeTexto;
+  }
+
+  ocultarError(): void {
+    this.htmlElement.nativeElement.classList.add('hidden')
+  }
+
+  mostrarError(): void {
+    this.htmlElement.nativeElement.classList.remove('hidden')
   }
 }
